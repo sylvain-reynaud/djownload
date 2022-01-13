f2b121d94f7ed81ec88b84d36677547933b97b6f
Sylvain Reynaud <sylvainreynaud@protonmail.com>
2022-01-13 23:04:15 +0100

    feature: songs are downloaded from an external API

--- README.md
+++ README.md
@@ -1,14 +1,23 @@
 # Music Playlist Downloader
 
+Project status : PoC
+
 ## Features
 
 - [x] Get a list of songs from a Youtube playlist
 - [x] CLI
 - [x] Output the list in a text file
+- [x] Download the songs with a web API
+- [x] Download progress bar
+- [ ] Write a documentation "How to use djownload"
 - [ ] Make different crates
-- [ ] Download the songs with a web API
-- [ ] Download the songs in a folder with the same name as the playlist
+- [ ] Properly handle errors, results
 - [ ] CI/CD on new releases : build and push docker image
+- [ ] Log messages for debugging [log crate](https://crates.io/crates/log)
+- [ ] Downloaded songs are placed in a folder with the same name as the playlist
+- [ ] CLI takes a playlist URL as an argument and not a playlist ID
+- [ ] Refactor the code to handle different download websites
+- [ ] Fallback to other download websites if the first one fails
 
 ## Usage
 
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,83 @@
+use serde_json::Value;
+use std::env;
 use std::fs::File;
 use std::io::Write;
 
 use async_recursion::async_recursion;
 use clap::Parser;
 use google_youtube3::YouTube;
-use google_youtube3::{Error, Result};
+use serde::{Deserialize, Serialize};
 use yup_oauth2::{InstalledFlowAuthenticator, InstalledFlowReturnMethod};
 
+use std::cmp::min;
+
+use futures_util::StreamExt;
+use indicatif::{ProgressBar, ProgressStyle};
+use reqwest::Client;
+
+pub async fn download_file(client: &Client, url: &str, path: &str) -> Result<(), String> {
+    let res = client
+        .get(url)
+        .header(
+            reqwest::header::USER_AGENT,
+            "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36",
+        )
+        .send()
+        .await
+        .or(Err(format!("Failed to GET from '{}'", &url)))?;
+    let total_size = res
+        .content_length()
+        .ok_or(format!("Failed to get content length from '{}'", &url))?;
+
+    let pb = ProgressBar::new(total_size);
+    pb.set_style(ProgressStyle::default_bar()
+        .template("{msg}\n{spinner:.green} [{elapsed_precise}] [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({bytes_per_sec}, {eta})")
+        .progress_chars("█  "));
+    pb.set_message(format!("Downloading {}", &url));
+
+    let mut file = File::create(path).or(Err(format!("Failed to create file '{}'", path)))?;
+    let mut downloaded: u64 = 0;
+    let mut stream = res.bytes_stream();
+
+    while let Some(item) = stream.next().await {
+        let chunk = item.or(Err(format!("Error while downloading file")))?;
+        file.write(&chunk)
+            .or(Err(format!("Error while writing to file")))?;
+        let new = min(downloaded + (chunk.len() as u64), total_size);
+        downloaded = new;
+        pb.set_position(new);
+    }
+
+    pb.finish_with_message(format!("Downloaded {}", path));
+    return Ok(());
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct SliderResult {
+    pub audios: Audios,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Audios {
+    #[serde(rename = "")]
+    pub field: Vec<GeneratedType>,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct GeneratedType {
+    pub id: String,
+    pub duration: i64,
+    #[serde(rename = "tit_art")]
+    pub tit_art: String,
+    pub url: String,
+    pub extra: Value,
+}
+
 #[derive(Parser)]
-#[clap(version = "0.1", author = "Polytech Montpellier - DevOps")]
+#[clap(version = "0.1", author = "@sylvainreynaud")]
 struct CLIOpts {
     /// Youtube Playlist ID
     playlist_id: String,
@@ -19,7 +88,9 @@ struct CLIOpts {
 }
 
 // Get the list of songs from a youtube playlist url
-async fn get_song_names_from_playlist_url(url: String) -> Result<Vec<String>> {
+async fn get_song_names_from_playlist_url(
+    url: String,
+) -> Result<Vec<String>, google_youtube3::Error> {
     // Read application secret from a file. Sometimes it's easier to compile it directly into
     // the binary. The clientsecret file contains JSON like `{"installed":{"client_id": ... }}`
     let secret = yup_oauth2::read_application_secret("clientsecret.json")
@@ -64,11 +135,11 @@ async fn get_song_names_from_playlist_url(url: String) -> Result<Vec<String>> {
             )
             .await
             {
-                return value;
+                return Ok(value?);
             }
             Ok(song_names)
         }
-        Err(e) => Err(Error::from(e)),
+        Err(e) => Err(google_youtube3::Error::from(e)),
     }
 }
 
@@ -78,7 +149,7 @@ async fn get_all_songs_from_pagination_recursive(
     song_names: &mut Vec<String>,
     hub: YouTube,
     url: String,
-) -> Option<Result<Vec<String>>> {
+) -> Option<google_youtube3::Result<Vec<String>>> {
     // extract_songs(&playlist_item_list_response, song_names);
 
     while playlist_item_list_response.next_page_token.is_some() {
@@ -112,7 +183,7 @@ async fn get_all_songs_from_pagination_recursive(
             }
             Err(e) => {
                 println!("Error: {:?}", e);
-                return Some(Err(Error::from(e)));
+                return Some(Err(google_youtube3::Error::from(e)));
             }
         }
     }
@@ -147,15 +218,18 @@ fn extract_songs(
 }
 
 #[tokio::main]
-async fn main() -> Result<()> {
+async fn main() -> google_youtube3::Result<()> {
     let opts: CLIOpts = CLIOpts::parse();
+
+    let download_host = env::var("DOWNLOAD_HOST").unwrap();
 
     let song_names = get_song_names_from_playlist_url(opts.playlist_id).await;
 
     match song_names {
         Ok(song_names) => {
             let mut clean_song_names = vec![];
-            for song_name in song_names {
+            for song_name in song_names.iter() {
+                // let song_name: String = song_name;
                 // list of string to remove
                 let to_remove_list = [
                     "\"",
@@ -176,11 +250,54 @@ async fn main() -> Result<()> {
                 ];
 
                 // for each to_remove items, remove it from song_name
-                let mut clean_song_name = song_name.clone();
+                let mut clean_song_name: String = song_name.clone();
                 for to_remote in to_remove_list {
                     clean_song_name = clean_song_name.replace(to_remote, "");
                 }
-                clean_song_names.push(clean_song_name);
+                clean_song_names.push(clean_song_name.clone());
+                println!("{}", &clean_song_name);
+
+                // Get JSON from https://xyz/vk_auth.php?q=<song_name>
+                let json_url = format!("{}vk_auth.php?q={}", download_host, clean_song_name);
+                println!("{}", &json_url);
+                // GET with headers
+                let client = reqwest::Client::new();
+                let response = client
+                    .get(&json_url)
+                    .header(
+                        reqwest::header::USER_AGENT,
+                        "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36",
+                    )
+                    .send()
+                    .await
+                    .unwrap();
+
+                let json: SliderResult = match serde_json::from_str(&response.text().await.unwrap())
+                {
+                    Ok(json) => json,
+                    Err(e) => {
+                        println!("No result, error: {:?}", e);
+                        continue;
+                    }
+                };
+
+                let selected_song = json.audios.field.into_iter().next().unwrap();
+                let download_url = format!(
+                    "{}download/{}/{}/{}/{}.mp3?extra=null",
+                    download_host,
+                    selected_song.id,
+                    selected_song.duration,
+                    selected_song.url,
+                    urlencoding::encode(&selected_song.tit_art),
+                );
+
+                // Download the song and save it to the current directory
+                download_file(
+                    &client,
+                    &download_url,
+                    &format!("download/{}.mp3", clean_song_name),
+                )
+                .await;
             }
 
             if opts.output_file.is_some() {
@@ -189,7 +306,7 @@ async fn main() -> Result<()> {
         }
         Err(e) => {
             println!("Error: {:?}", e);
-            return Err(Error::from(e));
+            return Err(google_youtube3::Error::from(e));
         }
     }
 
@@ -197,7 +314,10 @@ async fn main() -> Result<()> {
 }
 
 // Write clean_song_names into a txt file
-fn write_songs_to_file(output_file: String, clean_song_names: Vec<String>) -> Result<()> {
+fn write_songs_to_file(
+    output_file: String,
+    clean_song_names: Vec<String>,
+) -> google_youtube3::Result<()> {
     let mut file = File::create(output_file)?;
     file.write_all(clean_song_names.join("\n").as_bytes())?;
     Ok(())
